16579424d690d84b6e79db67546fa21402b5f8bb
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-03 12:33:52 -0500

    wrote all code and tested application

--- main.js
+++ main.js
@@ -13,14 +13,20 @@ console.log('##### Challenge 1 #####');
 //
 // Console out the numbers from 1-20, but only if they're divisible by 3.
 // Hint: Use `if` and a modulus operation.
-
+for (let i = 1; i <= 20; i++){
+    if (i % 3 === 0){
+        console.log(i);
+    }
+}
 
 
 console.log('##### Challenge 2 #####');
 // 2.
 //
 // Console out the numbers from 1-20. BACKWARDS.
-
+for (let i = 20; i > 0; i--){
+    console.log(i);
+}
 
 
 console.log('##### Challenge 3 #####');
@@ -29,7 +35,9 @@ console.log('##### Challenge 3 #####');
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
 let string3 = 'Boy howdy am I good at this!';
-
+for (let i = 0; i < string3.length; i++){
+    console.log(string3[i]);
+}
 
 
 console.log('##### Challenge 4 #####');
@@ -38,7 +46,9 @@ console.log('##### Challenge 4 #####');
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
 let string4 = 'And getting better every day.';
-
+for (let i = 1; i < string4.length; i +=3){
+    console.log(string4[i])
+}
 
 
 console.log('##### Challenge 5 #####');
@@ -47,7 +57,9 @@ console.log('##### Challenge 5 #####');
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
 let string5 = 'Am I the best?';
-
+for (let i = 0; i < string5.length; i++){
+    console.log(string5[i].toUpperCase())
+}
 
 
 console.log('##### Challenge 6 #####');
@@ -56,7 +68,12 @@ console.log('##### Challenge 6 #####');
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
 let string6 = 'Whoa, I am the best!';
-
+let str = string6.split(" ");
+str[2] = str[2].toUpperCase();
+str = str.join(" ");
+for (let i = 0; i < str.length; i++){
+    console.log(str[i]);
+}
 
 
 console.log('##### Challenge 7 #####');
@@ -66,7 +83,9 @@ console.log('##### Challenge 7 #####');
 //  individually, but backwards. That is, the first console log should be
 // `.`, followed by `s`, followed by `d`, and so on.
 let string7 = 'I am become death, destroyer of worlds.';
-
+for (let i = string7.length-1; i >= 0; i--){
+    console.log(string7[i])
+}
 
 
 console.log('##### Challenge 8 #####');
@@ -81,7 +100,13 @@ const names = 'Alex|Mesuara|Brian|Chanel|Matt|Jeremy B.|Jose|Andy|Dustin|Joshua|
 // You'll know you're at a new person's name when you hit the pipe character.
 
 // Don't print the pipes!
-
+let separate = names.split("|");
+for (let i = 0; i < separate.length; i ++){
+    console.log("Here today is:");
+    for(let j = 0; j < separate[i].length; j++){
+        console.log(separate[i][j]);
+    }
+}
 
 
 
